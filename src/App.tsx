b81b55e3a7150236fe0561880a911cfed96cc260
k-sang-soo <67569730+k-sang-soo@users.noreply.github.com>
2022-09-29 17:04:23 +0900

    readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Button from "./Button";
+import {GlobalStyle} from "./StyledReset";
 
 function App() {
   return (
       <>
-        <Button/>
+        <GlobalStyle/>
+        <Button width={"s"} height={"s"} text={"버튼"}/>
+        <Button width={"m"} height={"m"} text={"버튼"}/>
+        <Button width={"l"} height={"l"} text={"버튼"}/>
       </>
   );
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,8 +1,44 @@
+import styled from "styled-components";
 
+interface BtnProps {
+    width?: string;
+    height?: string;
+    bgColor?: string;
+    text?: string;
+}
 
-function Button() {
+const Btn = styled.button<BtnProps>`
+  position: relative;
+  display: block;
+  width: ${props =>
+          props.width === 's' ? '100px'
+                  : props.width === 'm' ? '140px'
+                          : props.width === 'l' && '180px'
+  };
+
+  height : ${props =>
+          props.height === 's' ? '40px'
+                  : props.height === 'm' ? '50px'
+                          : props.height === 'l' && '70px'
+  };
+  
+  transition: 0.3s ease-out;
+  border: 1px solid #000;
+  
+  
+  & + & {
+    margin-top: 10px;
+  }
+        
+`
+
+function Button({width, height, bgColor, text} : BtnProps) {
     return (
-        <button>버튼</button>
+        <>
+            <Btn width={width} height={height} bgColor={bgColor}>
+                {text}
+            </Btn>
+        </>
     );
 }
 
